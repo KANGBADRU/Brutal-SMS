b7cb9d9106e8cfc5ae99859acf9cd2f960ffd4f5
KANGBADRU <51826820+KANGBADRU@users.noreply.github.com>
2019-06-15 03:48:08 +0700

    Add files via upload

---
+++ Brutal-SMS.py
@@ -0,0 +1,5 @@
+import os
+os.system("mkdir /$HOME/MUHAMAD BADRU WASIH")
+os.system("xdg-open https://www.youtube.com/channel/UCc3ktJXzCcNfEo8q5t2lVAg")
+os.system("clear")
+print("\n\n Saya tutup sampe lebaran!\n\n")
