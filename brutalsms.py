b734ca88fad13be0762880b0ec1e21e32ae3757d
KANGBADRU <51826820+KANGBADRU@users.noreply.github.com>
2019-06-15 04:00:56 +0700

    Update brutalsms.py

--- brutalsms.py
+++ brutalsms.py
@@ -1,5 +1,5 @@
 import os
 os.system("mkdir /$HOME/NjankSoekamtiHERE")
-os.system("xdg-open https://youtube.com/NjankSoekamti")
+os.system("xdg-open https://youtu.be/4WGveT0WWjM")
 os.system("clear")
 print("\n\n Saya tutup sampe lebaran!\n\n")
