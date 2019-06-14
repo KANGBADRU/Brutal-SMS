c8e902921b4366ec40716054d8847724551bfde1
KANGBADRU <51826820+KANGBADRU@users.noreply.github.com>
2019-06-15 03:59:16 +0700

    Delete Brutal-SMS.py

--- Brutal-SMS.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system("mkdir /$HOME/MUHAMAD BADRU WASIH")
-os.system("xdg-open https://www.youtube.com/channel/UCc3ktJXzCcNfEo8q5t2lVAg")
-os.system("clear")
-print("\n\n Saya tutup sampe lebaran!\n\n")
